74ad7d674b3d0dca477335461855bce361082b17
Weakcods <bacayjhoshuajm@gmail.com>
2025-03-27 01:08:56 +0800

    update staticSTATIC_URL and STATICFILES_DIRS configuration

--- studentorg/studentorg/settings.py
+++ studentorg/studentorg/settings.py
@@ -118,11 +118,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = (
+STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
-)
+]
+
+# Add this to help Django find static files during development
+if DEBUG:
+    STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, 'static'),
+    ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
