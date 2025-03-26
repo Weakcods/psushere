6c08cb03ff56915df4a2db7a93cdb75077882425
Weakcods <bacayjhoshuajm@gmail.com>
2025-03-26 23:48:02 +0800

    add widget

--- studentorg/studentorg/settings.py
+++ studentorg/studentorg/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',  # Changed from 'studentorg.core' to 'core'
+    'widget_tweaks',  
 ]
 
 MIDDLEWARE = [
