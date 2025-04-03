41046b9c3513ceeaee8d6a9aeab2decb3dd97752
Weakcods <bacayjhoshuajm@gmail.com>
2025-04-03 14:26:05 +0800

    Update STATIC_ROOT path in settings.py

--- studentorg/studentorg/settings.py
+++ studentorg/studentorg/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/home/joshuawa/studentorg/staticfiles'
+STATIC_ROOT = '/home/joshettt/studentorg/staticfiles'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
