b378a05f0d82631566478b9fa8f06335990d9250
Weakcods <bacayjhoshuajm@gmail.com>
2025-04-03 10:50:40 +0800

    fix config

--- studentorg/studentorg/settings.py
+++ studentorg/studentorg/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-l@@we2gj8x4hq=)2x1kbn$ombgxxc)$+-jyp0k_j@#z#8&9led
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['joshett.pythonanywhere.com', '127.0.0.1']
 
 
 # Application definition
