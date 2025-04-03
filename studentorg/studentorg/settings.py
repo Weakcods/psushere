581e06fa218a4a973b26a059f542f941d977acd1
Weakcods <bacayjhoshuajm@gmail.com>
2025-04-03 11:48:21 +0800

    configure production settings and add WSGI file for deployment

--- studentorg/studentorg/settings.py
+++ studentorg/studentorg/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-l@@we2gj8x4hq=)2x1kbn$ombgxxc)$+-jyp0k_j@#z#8&9led'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['joshett.pythonanywhere.com', '127.0.0.1']
 
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
+    'widget_tweaks',  # Add this line
 ]
 
 MIDDLEWARE = [
@@ -118,15 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = '/home/joshuawa/studentorg/staticfiles'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
-
-if DEBUG:
-    STATICFILES_DIRS = [
-        os.path.join(BASE_DIR, 'static'),
-    ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
