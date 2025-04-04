a20a1fa52411e51b1e5aadd311a645eb92438f9f
Joshua Bacay <bacayjhoshuajm@gmail.com>
2025-04-04 18:43:55 +0800

    create new

--- studentorg/studentorg/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.views.generic import ListView
-from core.models import Organization  # Changed from studentorg.models to core.models
-
-class HomepageView(ListView):  # Changed from OrganizationListView to HomepageView
-    model = Organization
-    template_name = 'home.html'
-    context_object_name = 'home'
