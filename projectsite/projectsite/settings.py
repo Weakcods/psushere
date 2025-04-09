ab6132373d9c8935b0dfd66236f13d8f6e7a716c
Joshua Bacay <bacayjhoshuajm@gmail.com>
2025-04-09 09:32:29 +0800

    Update settings & base template for production readiness

--- projectsite/projectsite/settings.py
+++ projectsite/projectsite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-un%e0x7%_01$heigc#c13*k0vmkks+4^(m52i1^u&$o=y&^@&f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['joshettt.pythonanywhere.com', '127.0.0.1']
 
@@ -118,7 +119,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
 LOGIN_REDIRECT_URL = 'home'
 LOGIN_URL = 'login'
 
